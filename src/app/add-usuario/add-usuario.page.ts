087c31a33fb0567ee1fa9fe31989fb19072628fc
PEDRO DE ASSUNCAO ESTEVES <pedro.aesteves1@senacsp.edu.br>
2021-05-07 14:19:16 -0300

    entrega

---
+++ src/app/add-usuario/add-usuario.page.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/services/post';
+import { ToastController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-add-usuario',
+  templateUrl: './add-usuario.page.html',
+  styleUrls: ['./add-usuario.page.scss'],
+})
+export class AddUsuarioPage implements OnInit {
+
+  nome:string = "";
+  email:string = "";
+  senha:string = "";
+  cpf:string = "";
+  telefone:string = "";
+
+  constructor(private route:Router, private provider: Post , private routerAct : ActivatedRoute) { }
+
+  ngOnInit() {
+    // recuperando dados vindos pela rota
+    // this.routerAct.params.subscribe((data:any)=>{
+    //   this.id = data.id;
+    //   this.nome = data.nome;
+    //   this.usuario = data.usuario;
+    //   this.senha = data.senha;
+    //   this.nivel = data.nivel;
+    // });
+    //this.id = '1';
+  }
+
+  // Usuarios(){
+  //   this.route.navigate(['usuarios']);
+  // }
+
+  cadastrar(){
+    return new Promise(resolve =>{
+      let dados = {
+        requisicao: "add",
+        nome: this.nome,
+        email: this.email,
+        senha: this.senha,
+        cpf: this.cpf,
+        telefone: this.telefone  
+      };
+      this.provider.dadosApi(dados,'api.php').subscribe(data=>{
+        console.log(data);
+        this.route.navigate(['usuarios'])
+      });
+    })
+  }
+
+
+  editar(){
+//     return new Promise(resolve=>{
+//       let dados = {
+//         requisicao: 'editar',
+//         nome: this.nome,
+//         usuario: this.usuario,
+//         senha: this.senha,
+//         nivel: this.nivel,
+//         id: this.id
+//       };
+//       this.provider.dadosApi(dados,"api.php").subscribe(data=>{
+//         // faça aqui um teste para verificar se foi editado com sucesso
+//         // if(data['sucsess']){
+//           //
+//                   this.route.navigate[('usuarios')];
+//         //} else {
+//           // mostre uma mensagem
+// //        }
+
+//       });
+//     });
+    }
+
+  // constructor(){}
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
+    path: 'home',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'add-usuario',
+    loadChildren: () => import('./add-usuario/add-usuario.module').then( m => m.AddUsuarioPageModule)
   }
 ];
 
